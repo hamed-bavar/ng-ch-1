0573f1ccc7d61e40f495d8fd3602edad32c7f897
hamed <hamedbavarnegin@gmail.com>
2022-01-22 00:05:45 +0330

    Add loading service test

--- src/app/shared/services/loading.service.spec.ts
+++ src/app/shared/services/loading.service.spec.ts
@@ -13,4 +13,21 @@ describe('LoadingService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should set loading=true', () => {
+    let actualLoading: boolean | undefined;
+    service.getIsLoading().subscribe((isLoading) => {
+      actualLoading = isLoading;
+    });
+    service.setLoading();
+    expect(actualLoading).toBe(true);
+  });
+  it('should set loading=false', () => {
+    let actualLoading: boolean | undefined;
+    service.getIsLoading().subscribe((isLoading) => {
+      actualLoading = isLoading;
+    });
+    service.removeLoading();
+    expect(actualLoading).toBe(false);
+    expect(service.getIsLoading().value).toBe(false);
+  });
 });
